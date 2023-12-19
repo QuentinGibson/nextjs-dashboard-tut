6c2a29d0fd137e22166a1eace40e68446d2242f4
Quentin Gibson <quentingibson94@gmail.com>
2023-12-19 00:54:31 -0500

    Add loading component and dashboard page

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import DashboardSkeleton from "../../ui/skeletons";
+export default function Loading() {
+  /**
+   * Renders the loading state for the dashboard overview.
+   * @returns The loading skeleton component.
+   */
+  return <DashboardSkeleton />;
+}
